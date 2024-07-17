774c3cd38eee6c732230ed248db4d2dfc6db22d0
Gelis <wergelis@msn.com>
2024-07-17 11:50:40 +0800

    加入專案檔案。

---
+++ Blazor6HostBySrvLab1/Server/Services/ConfigureService.cs
@@ -0,0 +1,21 @@
+﻿namespace Blazor6HostBySrvLab1.Server.Services
+{
+    /// <summary>
+    /// Configure (AppSettings) 設定檔服務
+    /// </summary>
+    public class ConfigureService
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigureService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<string> GetConfigValue(string sectionName)
+        {
+            return Task.Factory.StartNew(() =>
+                _configuration.GetSection("appSettings").GetSection(sectionName).Value);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Blazor 6 的 AppSettings 測試程式
+
+## 1. 測試內嵌資源 appsetting.json
+
+## 2. ConfigureServices 讀取遠端資源
